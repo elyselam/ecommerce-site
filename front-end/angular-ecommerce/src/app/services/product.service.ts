00ca04ad685b18be66d217dab89d1b8062bfe37c
elyselam <elyse.p.lam@gmail.com>
2021-02-28 11:42:53 -0800

    added productService

--- front-end/angular-ecommerce/src/app/services/product.service.ts
+++ front-end/angular-ecommerce/src/app/services/product.service.ts
@@ -1,15 +1,28 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs";
+import {Observable, map} from "rxjs";
 import {Product} from "../common/product";
+import {map} from "rxjs/operator";
+
+interface GetResponse {
+  //unwrap json from Spring Data Rest _embedded entry
+  _embedded: {
+    products: Product[];
+  }
+}
 
 @Injectable({
   providedIn: 'root'
 })
+//make get request to backend, unwrap, make available as array of products
 export class ProductService {
   private baseUrl = 'http://localhost:8080/api/products';
   constructor(private httpClient: HttpClient) { }
-  getProductList(): Observable<Product[]>{
 
+  getProductList(): Observable<Product[]>{
+    return this.httpClient.get<GetResponse>(this.baseUrl)
+      .pipe(
+        map(response => response._embedded.products)
+    );
   }
 }
