f3bcbe241a89c557860a3201f5aa2bbf497fa7d2
elyselam <elyse.p.lam@gmail.com>
2021-02-27 22:31:59 -0800

    adding front-end folder

---
+++ front-end/angular-ecommerce/src/app/services/product.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Product} from "../common/product";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+  private baseUrl = 'http://localhost:8080/api/products';
+  constructor(private httpClient: HttpClient) { }
+  getProductList(): Observable<Product[]>{
+
+  }
+}
