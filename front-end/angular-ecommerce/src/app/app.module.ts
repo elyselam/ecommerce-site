b1310c084300cf4688a201ff6cb4db1fd946dc24
elyselam <elyse.p.lam@gmail.com>
2021-03-08 20:58:18 -0800

    adding router

--- front-end/angular-ecommerce/src/app/app.module.ts
+++ front-end/angular-ecommerce/src/app/app.module.ts
@@ -2,9 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './product-list/product-list.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
 import {HttpClientModule} from "@angular/common/http";
 import {ProductService} from "./services/product.service";
+import {RouterModule, Routes} from "@angular/router";
+
+//when path matches, create a new instance of component
+//goes most specific to most generic
+const routes: Routes = [
+  {path: 'category/:id', component: ProductListComponent},
+  {path: 'category', component: ProductListComponent },
+  {path: 'products', component: ProductListComponent },
+  {path: '', redirectTo: '/products', pathMatch: 'full'},
+  {path: '**', redirectTo: '/products', pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
@@ -14,7 +25,8 @@ import {ProductService} from "./services/product.service";
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
