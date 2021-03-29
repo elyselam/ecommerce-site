df8af0c1e77d9721666da7c752ae6a8b9ca7fa19
elyselam <elyse.p.lam@gmail.com>
2021-03-28 18:18:10 -0700

    bootstrap works phewwww

--- front-end/angular-ecommerce/src/app/app.module.ts
+++ front-end/angular-ecommerce/src/app/app.module.ts
@@ -11,7 +11,7 @@ import { SearchComponent } from './components/search/search.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { CartStatusComponent } from './componenets/cart-status/cart-status.component';
+import { CartStatusComponent } from './components/cart-status/cart-status.component';
 
 //when path matches, create a new instance of component
 //goes most specific to most generic
